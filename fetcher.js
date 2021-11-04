4b12686f68461367e029b67386722474f9d8ff20
belalelmi <belalelmi@gmail.com>
2021-11-03 22:08:08 -0400

    inital commit

--- fetcher.js
+++ fetcher.js
@@ -7,16 +7,12 @@ const url = process.argv[2]
 request(url, (error, response, body) => {
 
   fs.writeFile(`${localFilePath}`, body, function (error) {
-    console.log('statusCode:', response.statusCode);
-    console.log(`Downloaded and saved ${response.headers["content-length"]} bytes to ` + process.argv[3])
+    if (error) {
+      console.log('Whoops:' + error)
+    } else {
+      console.log('statusCode:', response.statusCode);
+      console.log(`Downloaded and saved ${response.headers["content-length"]} bytes to ` + process.argv[3]);
+    }
   }
-
   )
-})
-
-// request('http://www.example.edu/', (error, response, body) => {
-//   // console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   // console.log('body:', body); // Print the HTML for the Google homepage.
-// });
-
+});
